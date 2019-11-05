7f8260d585fece719c809890aeff9a52ffa9bda0
Mykhailo <Mykhailo_Mishyn@epam.com>
2019-11-05 13:51:43 +0200

    Update class Factory

--- src/classes/Factory.js
+++ src/classes/Factory.js
@@ -1,4 +1,5 @@
 const Point = require('./Point');
+const Edge = require('./Edge');
 
 module.exports = function Factory() {
 
@@ -8,9 +9,24 @@ module.exports = function Factory() {
             if(props instanceof Point) return new Point(props.x, props.y);
             if(props){
                 if(props.x || props.y) return new Point(props.x || 0, props.y || 0);
-                throw 'Wrong point creation params';
+                throw 'Wrong Point creation params';
             }
             return new Point(0, 0);
+        },
+
+        createEdge: function (props) {
+            if(props instanceof Edge) return new Edge(props.start.x, props.start.y, props.end.x, props.end.y);
+            if(props){
+
+                const x1 = props.x1 || 0;
+                const y1 = props.y1 || 0;
+                const x2 = props.x2 || 0;
+                const y2 = props.y2 || 0;
+
+                if(x1 === x2 && y1 === y2) throw "Points are equal or wrong Edge creation param given";
+                return new Edge(x1, y1, x2, y2);
+            }
+            throw "No arguments";
         }
     }
 };
--- src/classes/test/Factory.test.js
+++ src/classes/test/Factory.test.js
@@ -4,49 +4,127 @@ const factory = Factory();
 
 describe('class Factory', () => {
 
-    describe('creates Point with', () => {
+    describe('createPoint', () => {
 
-        test('all params', () => {
-            const point = factory.createPoint({x: 1, y: 1});
-            const result = {x: point.x, y: point.y};
-            const expected = {x: 1, y: 1};
-            expect(result).toMatchObject(expected);
+        describe('creates Point with', () => {
+
+            test('all params', () => {
+                const point = factory.createPoint({x: 1, y: 1});
+                const result = {x: point.x, y: point.y};
+                const expected = {x: 1, y: 1};
+                expect(result).toMatchObject(expected);
+            });
+
+            test('x param', () => {
+                const point = factory.createPoint({x: 1});
+                const result = {x: point.x, y: point.y};
+                const expected = {x: 1, y: 0};
+                expect(result).toMatchObject(expected);
+            });
+
+            test('y param', () => {
+                const point = factory.createPoint({y: 1});
+                const result = {x: point.x, y: point.y};
+                const expected = {x: 0, y: 1};
+                expect(result).toMatchObject(expected);
+            });
+
+            test('no params', () => {
+                const point = factory.createPoint();
+                const result = {x: point.x, y: point.y};
+                const expected = {x: 0, y: 0};
+                expect(result).toMatchObject(expected);
+            });
+
+            test('Point param', () => {
+                const pointToCopy = factory.createPoint({x: 1, y: 1});
+                const point = factory.createPoint(pointToCopy);
+                const result = {x: point.x, y: point.y};
+                const expected = {x: 1, y: 1};
+                expect(result).toMatchObject(expected);
+            })
         });
 
-        test('x param', () => {
-            const point = factory.createPoint({x: 1});
-            const result = {x: point.x, y: point.y};
-            const expected = {x: 1, y: 0};
-            expect(result).toMatchObject(expected);
-        });
-
-        test('y param', () => {
-            const point = factory.createPoint({y: 1});
-            const result = {x: point.x, y: point.y};
-            const expected = {x: 0, y: 1};
-            expect(result).toMatchObject(expected);
-        });
-
-        test('no params', () => {
-            const point = factory.createPoint();
-            const result = {x: point.x, y: point.y};
-            const expected = {x: 0, y: 0};
-            expect(result).toMatchObject(expected);
-        });
-
-        test('Point param', () => {
-            const pointToCopy = factory.createPoint({x: 1, y: 1});
-            const point = factory.createPoint(pointToCopy);
-            const result = {x: point.x, y: point.y};
-            const expected = {x: 1, y: 1};
-            expect(result).toMatchObject(expected);
+        test('throws exception', () => {
+            const wrongParam = "Wrong";
+            expect(() => {
+                factory.createPoint(wrongParam)
+            }).toThrow('Wrong Point creation param')
         })
     });
 
-    test('throws exception', () => {
-        const wrongParam = "Wrong";
-        expect(() => {
-            factory.createPoint(wrongParam)
-        }).toThrow('Wrong point creation params')
+    describe('createEdge', () => {
+
+        describe('creates Edge with', () => {
+
+            test('all params', () => {
+                const edge = factory.createEdge({x1: 1, y1: 1, x2: 2, y2: 2});
+                const result = {x1: edge.start.x, y1: edge.start.y, x2: edge.end.x, y2: edge.end.y};
+                const expected = {x1: 1, y1: 1, x2: 2, y2: 2};
+                expect(result).toMatchObject(expected);
+            });
+
+            test('start point params', () => {
+                const edge = factory.createEdge({x1: 1, y1: 1});
+                const result = {x1: edge.start.x, y1: edge.start.y, x2: edge.end.x, y2: edge.end.y};
+                const expected = {x1: 1, y1: 1, x2: 0, y2: 0};
+                expect(result).toMatchObject(expected);
+            });
+
+            test('end points params', () => {
+                const edge = factory.createEdge({x2: 2, y2: 2});
+                const result = {x1: edge.start.x, y1: edge.start.y, x2: edge.end.x, y2: edge.end.y};
+                const expected = {x1: 0, y1: 0, x2: 2, y2: 2};
+                expect(result).toMatchObject(expected);
+            });
+
+            test('start x param', () => {
+                const edge = factory.createEdge({x1: 1});
+                const result = {x1: edge.start.x, y1: edge.start.y, x2: edge.end.x, y2: edge.end.y};
+                const expected = {x1: 1, y1: 0, x2: 0, y2: 0};
+                expect(result).toMatchObject(expected);
+            });
+
+            test('start y param', () => {
+                const edge = factory.createEdge({y1: 1});
+                const result = {x1: edge.start.x, y1: edge.start.y, x2: edge.end.x, y2: edge.end.y};
+                const expected = {x1: 0, y1: 1, x2: 0, y2: 0};
+                expect(result).toMatchObject(expected);
+            });
+
+            test('end x param', () => {
+                const edge = factory.createEdge({x2: 2});
+                const result = {x1: edge.start.x, y1: edge.start.y, x2: edge.end.x, y2: edge.end.y};
+                const expected = {x1: 0, y1: 0, x2: 2, y2: 0};
+                expect(result).toMatchObject(expected);
+            });
+
+            test('end y param', () => {
+                const edge = factory.createEdge({y2: 2});
+                const result = {x1: edge.start.x, y1: edge.start.y, x2: edge.end.x, y2: edge.end.y};
+                const expected = {x1: 0, y1: 0, x2: 0, y2: 2};
+                expect(result).toMatchObject(expected);
+            });
+
+            test('Edge param', () => {
+                const edgeToCopy = factory.createEdge({x1: 1, y1: 1, x2: 2, y2: 2});
+                const edge = factory.createEdge(edgeToCopy);
+                const result = {x1: edge.start.x, y1: edge.start.y, x2: edge.end.x, y2: edge.end.y};
+                const expected = {x1: 1, y1: 1, x2: 2, y2: 2};
+                expect(result).toMatchObject(expected);
+            })
+        });
+
+        describe('throws for', () => {
+
+            test('no args', () => {
+                expect(() => {factory.createEdge()}).toThrow('No arguments');
+            });
+
+            test('wrong param', () => {
+                const wrongParam = "Wrong";
+                expect(() => {factory.createEdge(wrongParam)}).toThrow('Points are equal or wrong Edge creation param given');
+            });
+        })
     })
 });
